3085548380810a6d7063b3ad4cdaa11efcc08f9b
prohv <socialhv1@gmail.com>
2025-11-21 23:59:24 +0530

    add: function to initialise spell check

--- background.js
+++ background.js
@@ -1,9 +1,19 @@
 import NSpell from 'nspell';
 
-let spellchecker = null;
+let spellchecker = null; //var to store spellcheck instance
+
 async function initializeSpellChecker(){
     try {
         console.log("Initialising spell check...");
-        
+        const affResponse = await fetch(chrome.runtime.getURL('dictionaries/en_US.aff'));
+        const dicResponse = await fetch(chrome.runtime.getURL('dictionaries/en_US.dic'));
+        const affContent = await affResponse.text();
+        const dicContent = await dicResponse.text();
+        spellchecker = new NSpell(affContent, dicContent);
+        console.log("Spell check initialised successfully.");
+        return true;
+    } catch (error) {
+        console.error("Failed to initialise spell check:", error);
+        return false;
     }
 }
